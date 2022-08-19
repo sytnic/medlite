3443951f083cbe680cdc39f8d312a2c8a2da9400
sytnic <sytnic@list.ru>
2022-08-19 10:37:35 +0300

    Step 1. v2. Clean up.

--- public/client_data.php
+++ public/client_data.php
@@ -4,16 +4,6 @@
       require_once("../includes/validation_functions.php");
 ?>
 <?php
-// Стираю сессию по get-запросу.
-// Работает совместно с Кнопкой стирания сессии,
-// но в текущей версии 
-// (возврат со следующего шага и автоматическое стирание сессии)
-// этот блок не нужен.
-if (isset($_GET["sess"]) && $_GET["sess"] == "zero") {
-    // echo $_GET["sess"];
-    $_SESSION["inputs"] = null;
-}
-
 // инициализация переменных для html
 $firstname = "";
 $midname = "";
@@ -23,7 +13,7 @@ $phone = "";
 
 if (isset($_POST['submit'])) {
 
-  // get variables
+  // getting variables
     $firstname = mysql_prep($_POST["firstname"]);
     $midname = mysql_prep($_POST["midname"]);
     $lastname = mysql_prep($_POST["lastname"]);
@@ -38,39 +28,9 @@ if (isset($_POST['submit'])) {
 	  validate_presences($required_fields);
    
   if (empty($errors)) {
-
       $_SESSION["inputs"] = [$firstname, $midname, $lastname, $birthday, $phone];
       redirect_to("choice_spec.php");
-  
-    /*  
-      // query  
-        $query  = "INSERT INTO clients (";
-        $query .= "first_name, middle_name, surname, datebirth, phone";
-        $query .= ") VALUES (";
-        $query .= " '{$firstname}', '{$midname}', '{$lastname}', '{$birthday}', '{$phone}'";
-        $query .= ")";
-        $result = mysqli_query($connection, $query);
-
-      // check query
-        if ($result) {
-          // Success
-          redirect_to("choice_spec.php");
-        } else {
-          // Failure
-            if(mysqli_errno($connection)) {
-              echo "Error: " . 
-                  // not for production:
-                  mysqli_error($connection) . 
-                  // for production:
-                  " (" . mysqli_errno($connection) . "). "
-              ;
-              // not for production:
-              echo $query;
-            }
-        }
-    */
-
-  }  // end of - if (empty($errors))
+  }
     
 } else {
   // Вероятно, GET запрос
@@ -96,21 +56,8 @@ include("layouts/sidebar.php");
 
 <p>Please, fill the form.</p>
 
-<?php  // Кнопка стирания сессии,
-      // появляется, если есть сессия на странице
-
-      // if(isset($_SESSION["inputs"])) { ?>
-      <!--      <p><a href="client_data.php?sess=zero" class="w3-button w3-brown">Erase my data</a></p>  -->
-<?php // } ?>
-
 <p>
-  <?php
-      /*
-        var_dump($_SESSION["inputs"]);
-        var_dump($_SESSION["inputs"][0]);      
-        if(isset($_SESSION["inputs"])) {echo $_SESSION["inputs"][0]; } 
-        echo "<br>" ; 
-      */
+  <?php     
         // нужно инициализировать пустые строки выше под эти переменные, 
         // раз эти переменные используются
         echo "Var name: $firstname. <br>";
@@ -121,19 +68,8 @@ include("layouts/sidebar.php");
         echo "<pre>";
         print_r($_SESSION["inputs"]);
         echo "</pre>";
-
   ?>
-  <?php echo form_errors($errors); 
-        
-        /*
-        проблема #1.
-        текущее поведение (Step 1. v1): 
-          если заполнить форму, вернуться на неё, стереть значение и отправить форму,
-          то отправка будет предотвращена php-валидацией, 
-          но значение будет заполнено из сессии
-        */  
-  ?>
-  
+  <?php echo form_errors($errors); ?>  
 </p>
 
   <form action="client_data.php" method="post" class="w3-container w3-card-4 w3-light-grey w3-text-green w3-margin">
@@ -165,16 +101,7 @@ include("layouts/sidebar.php");
           <input class="w3-input w3-border" name="lastname" type="text" placeholder="Last Name *" 
           value="<?php echo (isset($_SESSION["inputs"])) ? $_SESSION["inputs"][2] : $lastname; ?>" >
         </div>
-    </div>        
-    
-    <!--
-    <div class="w3-row w3-section">
-      <div class="w3-col " style="width:50px"><i class="w3-xxlarge fa fa-user"></i></div>
-        <div class="w3-rest" style="width: 50%;">
-          <input class="w3-input w3-border" name="message" type="number" min="1" max="99" placeholder="Age">
-        </div>
     </div>
-    -->
               
     <div class="w3-row w3-section">
       <div class="w3-col " style="width:50px"><i class="w3-xxlarge fa fa-user"></i></div>
@@ -198,9 +125,9 @@ include("layouts/sidebar.php");
   </form>    
 
 <?php
-    // Стираю сессию по get-запросу,
-    // если вернулся со следующего шага.
-    // Это нужно для решения проблемы #1.
+    // По get-запросу стираю сессию и её значения 
+    // после первого появления в форме,
+    // когда вернулся со следующего шага.
     if (isset($_GET["from"]) && $_GET["from"] == "step2") {
       $_SESSION["inputs"] = null;
     }
